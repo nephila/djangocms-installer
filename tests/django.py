67a2275500ce0ca27c42a0951752a4369caab874
Iacopo Spalletti <i.spalletti@nephila.it>
2013-09-08 09:12:17 +0200

    Fix tests

--- tests/django.py
+++ tests/django.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 import sys
 import os.path
-import tempfile
+import six
 import sqlite3
+from . import unittest
 
 from aldryn_installer import config, django, install
 from . import BaseTestClass
@@ -12,19 +13,21 @@ from . import BaseTestClass
 class TestDjango(BaseTestClass):
 
     def test_create_project(self):
-        self.project_dir = tempfile.mkdtemp()
+        self._remove_project_dir()
         config_data = config.parse(['--db=postgres://user:pwd@host/dbname',
-                                    '-q', '-p'+self.project_dir, 'test_project'])
+                                    '--cms-version=develop',
+                                    '-q', '-p'+self.project_dir, 'example_prj'])
         install.requirements(config_data.requirements)
         django.create_project(config_data)
-        self.assertTrue(os.path.exists(os.path.join(self.project_dir, 'test_project')))
+        self.assertTrue(os.path.exists(os.path.join(self.project_dir, 'example_prj')))
 
     def test_patch(self):
-        self.project_dir = tempfile.mkdtemp()
+        self._remove_project_dir()
         config_data = config.parse(['--db=sqlite://localhost/test.db',
                                     '--lang=en',
+                                    '--cms-version=develop',
                                     '-f', '-q', '-u', '-zno', '--i18n=no',
-                                    '-p'+self.project_dir, 'test_project'])
+                                    '-p'+self.project_dir, 'example_patch'])
         install.requirements(config_data.requirements)
         django.create_project(config_data)
         django.patch_settings(config_data)
@@ -34,32 +37,41 @@ class TestDjango(BaseTestClass):
 
         # settings is importable even in non django environment
         sys.path.append(config_data.project_directory)
-        project = __import__('%s' % config_data.project_name,
-                                  globals(), locals(), ['settings'])
-        # checking for django options
+
+        project = __import__(config_data.project_name,
+                             globals(), locals(), ['settings'])
+
+        ## checking for django options
         self.assertFalse(project.settings.USE_L10N)
         self.assertFalse(project.settings.USE_TZ)
         self.assertEqual(project.settings.LANGUAGE_CODE, 'en')
-
-        # checking for standard CMS settings
+        #
+        ## checking for standard CMS settings
         self.assertTrue('sekizai.context_processors.sekizai' in project.settings.TEMPLATE_CONTEXT_PROCESSORS)
         self.assertTrue('cms.middleware.toolbar.ToolbarMiddleware' in project.settings.MIDDLEWARE_CLASSES)
         self.assertTrue(project.settings.CMS_LANGUAGES['default']['redirect_on_fallback'])
         self.assertEqual(project.settings.CMS_LANGUAGES[1][0]['code'], 'en')
 
-        # checking for filer (optional) settings
+        ## checking for filer (optional) settings
         self.assertTrue('filer' in project.settings.INSTALLED_APPS)
         self.assertTrue('easy_thumbnails' in project.settings.INSTALLED_APPS)
         self.assertTrue(hasattr(project.settings, 'THUMBNAIL_PROCESSORS'))
 
-        # basic urlconf check
+        ## basic urlconf check
         self.assertTrue('cms.urls' in urlconf)
         self.assertTrue('staticfiles_urlpatterns' in urlconf)
 
-    def test_setup_database(self):
-        self.project_dir = tempfile.mkdtemp()
+        sys.path.remove(config_data.project_directory)
+        self._remove_project_dir()
+        del project
+        del(sys.modules["%s.settings" % config_data.project_name])
+
+    @unittest.skipIf(six.PY3, "Filer not compatible with Python 3, skipping")
+    def test_setup_database_filer(self):
+        self._remove_project_dir()
         config_data = config.parse(['--db=sqlite://localhost/test.db',
                                     '-f', '-q', '-u',
+                                    '--cms-version=develop',
                                     '-p'+self.project_dir, 'aldryn_project'])
         install.requirements(config_data.requirements)
         django.create_project(config_data)
@@ -78,3 +90,25 @@ class TestDjango(BaseTestClass):
         # schema should be fine
         query = project_db.execute('SELECT * FROM cms_page')
         self.assertTrue(query)
+
+    def test_setup_database(self):
+        self._remove_project_dir()
+        config_data = config.parse(['--db=sqlite://localhost/test.db',
+                                    '-q', '-u', '--cms-version=develop',
+                                    '-p'+self.project_dir, 'aldryn_project'])
+        install.requirements(config_data.requirements)
+        django.create_project(config_data)
+        django.patch_settings(config_data)
+        django.copy_files(config_data)
+        django.setup_database(config_data)
+        project_db = sqlite3.connect(os.path.join(config_data.project_directory, 'test.db'))
+
+        # Checking content type table to check for correct applications setup
+        query = project_db.execute('SELECT * FROM django_content_type WHERE app_label="cms" AND model="page"')
+        self.assertEqual(query.fetchone()[1], 'page')
+
+        # No data in CMS tables at setup time, but if query succeed database
+        # schema should be fine
+        query = project_db.execute('SELECT * FROM cms_page')
+        self.assertTrue(query)
+
--- tests/main.py
+++ tests/main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
-import tempfile
-import shutil
+import os
 
 from aldryn_installer import main
 from . import BaseTestClass, PatchStd
@@ -11,50 +10,50 @@ class TestMain(BaseTestClass):
 
     def test_main_invocation(self):
         with PatchStd(self.stdout, self.stderr):
-            self.project_dir = tempfile.mkdtemp()
+            self._remove_project_dir()
             sys.argv = ['main'] + ['--db=sqlite://localhost/test.db',
-                                   '-len',
-                                   '-q', '-u', '-p'+self.project_dir, 'test_project']
+                                   '-len', '--cms-version=develop',
+                                   '-q', '-u', '-p'+self.project_dir, 'example_prj']
             main.execute()
             # Checking we successfully completed the whole process
-            self.assertTrue("Get into '%s' directory and type 'python manage.py runserver' to start your project" % self.project_dir in self.stdout.getvalue())
+            self.assertTrue(("Get into '%s' directory and type 'python manage.py runserver' to start your project" % self.project_dir) in self.stdout.getvalue())
 
     def test_two_langs_invocation(self):
         with PatchStd(self.stdout, self.stderr):
-            self.project_dir = tempfile.mkdtemp()
+            self._remove_project_dir()
             sys.argv = ['main'] + ['--db=sqlite://localhost/test.db',
-                                   '-len', '-lfr',
-                                   '-q', '-u', '-p'+self.project_dir, 'test_project']
+                                   '-len', '-lfr', '--cms-version=develop',
+                                   '-q', '-u', '-p'+self.project_dir, 'example_prj']
             main.execute()
             # Checking we successfully completed the whole process
-            self.assertTrue("Get into '%s' directory and type 'python manage.py runserver' to start your project" % self.project_dir in self.stdout.getvalue())
+            self.assertTrue(("Get into '%s' directory and type 'python manage.py runserver' to start your project" % self.project_dir) in self.stdout.getvalue())
 
     def manual_test_develop(self):
         with PatchStd(self.stdout, self.stderr):
-            self.project_dir = tempfile.mkdtemp()
+            self._remove_project_dir()
             sys.argv = ['main'] + ['--db=sqlite://localhost/test.db',
                                    '-len', '--cms-version=develop',
-                                   '-q', '-u', '-p'+self.project_dir, 'test_project']
+                                   '-q', '-u', '-p'+self.project_dir, 'example_prj']
             main.execute()
             # Checking we successfully completed the whole process
-            self.assertTrue("Get into '%s' directory and type 'python manage.py runserver' to start your project" % self.project_dir in self.stdout.getvalue())
+            self.assertTrue(("Get into '%s' directory and type 'python manage.py runserver' to start your project" % self.project_dir) in self.stdout.getvalue())
 
     def manual_test_django_1_4(self):
         with PatchStd(self.stdout, self.stderr):
-            self.project_dir = tempfile.mkdtemp()
+            self._remove_project_dir()
             sys.argv = ['main'] + ['--db=sqlite://localhost/test.db',
                                    '-len', '--django-version=1.4',
-                                   '-q', '-u', '-p'+self.project_dir, 'test_project']
+                                   '-q', '-u', '-p'+self.project_dir, 'example_prj']
             main.execute()
             # Checking we successfully completed the whole process
-            self.assertTrue("Get into '%s' directory and type 'python manage.py runserver' to start your project" % self.project_dir in self.stdout.getvalue())
+            self.assertTrue(("Get into '%s' directory and type 'python manage.py runserver' to start your project" % self.project_dir) in self.stdout.getvalue())
 
     def manual_test_django_1_5(self):
         with PatchStd(self.stdout, self.stderr):
-            self.project_dir = tempfile.mkdtemp()
+            self._remove_project_dir()
             sys.argv = ['main'] + ['--db=sqlite://localhost/test.db',
                                    '-len', '--django-version=1.5',
-                                   '-q', '-u', '-p'+self.project_dir, 'test_project']
+                                   '-q', '-u', '-p'+self.project_dir, 'example_prj']
             main.execute()
             # Checking we successfully completed the whole process
-            self.assertTrue("Get into '%s' directory and type 'python manage.py runserver' to start your project" % self.project_dir in self.stdout.getvalue())
+            self.assertTrue(("Get into '%s' directory and type 'python manage.py runserver' to start your project" % self.project_dir) in self.stdout.getvalue())
