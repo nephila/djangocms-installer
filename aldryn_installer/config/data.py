1807b73fcc76f63d2e10184690cb420b96b93588
Kim Thoenen <kim@smuzey.ch>
2014-02-06 14:41:35 +0100

    Push cms beta version to 3.0b4.dev5
    See https://github.com/divio/django-cms/commit/042f4953f6d2db66fd15e4cb748ab4b279f57947

--- aldryn_installer/config/data.py
+++ aldryn_installer/config/data.py
@@ -6,7 +6,7 @@ CONFIGURABLE_OPTIONS = ['--db', '--cms-version', '--django-version', '--i18n',
                         '--permissions', '--bootstrap', '--starting-page']
 
 DJANGOCMS_DEVELOP = 'https://github.com/divio/django-cms/archive/develop.zip'
-DJANGOCMS_BETA = 'https://github.com/divio/django-cms/archive/3.0.0.beta3.zip'
+DJANGOCMS_BETA = 'https://github.com/divio/django-cms/archive/042f4953f6d2db66fd15e4cb748ab4b279f57947.zip'
 DJANGOCMS_LATEST = '2.4'
 
 DJANGO_DEVELOP = 'https://github.com/django/django/archive/master.zip'
