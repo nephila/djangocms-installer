ef7efd16ce7ef31df336d7509ce65ca5d2d1ffce
Iacopo Spalletti <i.spalletti@nephila.it>
2014-11-30 13:22:34 +0100

    Update classifier

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     zip_safe=False,
     keywords='djangocms-installer',
     classifiers=[
-        'Development Status :: 5 - Production',
+        'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
