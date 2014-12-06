d7e2766966eff8a42ee35e3f9e902d1564e2098a
Iacopo Spalletti <i.spalletti@nephila.it>
2014-12-06 20:33:39 +0100

    Fix MIGRATION_MODULES_COMMON

--- djangocms_installer/config/settings.py
+++ djangocms_installer/config/settings.py
@@ -192,14 +192,14 @@ SOUTH_MIGRATION_MODULES = (
 MIGRATION_MODULES_COMMON = (
     ('djangocms_text_ckeditor', 'djangocms_text_ckeditor.migrations_django'),
     ('djangocms_column', 'djangocms_column.migrations_django'),
+    ('djangocms_flash', 'djangocms_flash.migrations_django'),
+    ('djangocms_googlemap', 'djangocms_googlemap.migrations_django'),
     ('djangocms_inherit', 'djangocms_inherit.migrations_django'),
     ('djangocms_style', 'djangocms_style.migrations_django'),
 )
 
 MIGRATION_MODULES_STANDARD = (
     ('djangocms_file', 'djangocms_file.migrations_django'),
-    ('djangocms_flash', 'djangocms_flash.migrations_django'),
-    ('djangocms_googlemap', 'djangocms_googlemap.migrations_django'),
     ('djangocms_link', 'djangocms_link.migrations_django'),
     ('djangocms_picture', 'djangocms_picture.migrations_django'),
     ('djangocms_teaser', 'djangocms_teaser.migrations_django'),
