68d0e2ddcf700b40c908cab9ede601c93c706fb3
Iacopo Spalletti <i.spalletti@nephila.it>
2015-01-25 11:10:56 +0100

    Better information to user during project setup

--- djangocms_installer/main.py
+++ djangocms_installer/main.py
@@ -19,11 +19,15 @@ def execute():
         elif config_data.dump_reqs:
             config.show_requirements(config_data)
         else:
+            sys.stdout.write("Creating the project\n"
+                             "Please wait while I install dependencies\n")
             if not config_data.no_deps:
                 if config_data.requirements_file:
                     install.requirements(config_data.requirements_file, True)
                 else:
                     install.requirements(config_data.requirements)
+            sys.stdout.write("Dependencies installed\n"
+                             "Creating the project")
             install.check_install(config_data)
             django.create_project(config_data)
             django.patch_settings(config_data)
@@ -34,14 +38,16 @@ def execute():
                 django.load_starting_page(config_data)
             if config_data.aldryn:
                 sys.stdout.write("Project created!\n")
-                sys.stdout.write("aldryn boilerplate requires action before you can actually run "
-                                 "the project.\n"
-                                 "See documentation at http://aldryn-boilerplate.readthedocs.org/"
+                sys.stdout.write("aldryn boilerplate requires action before "
+                                 "you can actually run the project.\n"
+                                 "See documentation at "
+                                 "http://aldryn-boilerplate.readthedocs.org/"
                                  "for more information.\n")
             else:
                 sys.stdout.write("All done!\n")
-                sys.stdout.write("Get into '%s' directory and type 'python manage.py runserver' "
-                                 "to start your project\n" % os.path.abspath(config_data.project_directory))
+                sys.stdout.write("Get into '%s' directory and type "
+                                 "'python manage.py runserver' to start your "
+                                 "project\n" % os.path.abspath(config_data.project_directory))
     except Exception as e:
         # Clean up your own mess
         install.cleanup_directory(config_data)
