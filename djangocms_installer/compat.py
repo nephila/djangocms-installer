e73ce5f8e23e660cf2fb04f9030d61334a1994ec
Iacopo Spalletti <i.spalletti@nephila.it>
2015-12-13 07:47:30 +0100

    Monkeypatch check_output for 2.6

--- djangocms_installer/compat.py
+++ djangocms_installer/compat.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
+import subprocess
 import six
 
 if six.PY3:
@@ -28,3 +29,18 @@ else:
 iteritems = six.iteritems
 
 StringIO = six.StringIO
+
+if 'check_output' not in dir(subprocess): # duck punch it in!
+    def f(*popenargs, **kwargs):
+        if 'stdout' in kwargs:
+            raise ValueError('stdout argument not allowed, it will be overridden.')
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get('args')
+            if cmd is None:
+                cmd = popenargs[0]
+            raise subprocess.CalledProcessError(retcode, cmd)
+        return output
+    subprocess.check_output = f
