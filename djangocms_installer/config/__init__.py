6819db737b9eb2192d8ea226b797f04ec92378af
Iacopo Spalletti <i.spalletti@nephila.it>
2016-02-05 19:00:32 +0100

    Small refactoring

--- djangocms_installer/config/__init__.py
+++ djangocms_installer/config/__init__.py
@@ -157,49 +157,7 @@ def parse(args):
                          'is exists.\n' % args.config_dump)
         sys.exit(8)
 
-    for item in data.CONFIGURABLE_OPTIONS:
-        action = parser._option_string_actions[item]
-        choices = default = ''
-        input_value = getattr(args, action.dest)
-        new_val = None
-        # cannot count this until we find a way to test input
-        if not args.noinput:  # pragma: no cover
-            if action.choices:
-                choices = ' (choices: %s)' % ', '.join(action.choices)
-            if input_value:
-                if type(input_value) == list:
-                    default = ' [default %s]' % ', '.join(input_value)
-                else:
-                    default = ' [default %s]' % input_value
-
-            while not new_val:
-                prompt = '%s%s%s: ' % (action.help, choices, default)
-                if action.choices in ('yes', 'no'):
-                    new_val = utils.query_yes_no(prompt)
-                else:
-                    new_val = compat.input(prompt)
-                new_val = compat.clean(new_val)
-                if not new_val and input_value:
-                    new_val = input_value
-                if new_val and action.dest == 'templates':
-                    if new_val != 'no' and not os.path.isdir(new_val):
-                        sys.stdout.write('Given directory does not exists, retry\n')
-                        new_val = False
-                if new_val and action.dest == 'db':
-                    action(parser, args, new_val, action.option_strings)
-                    new_val = getattr(args, action.dest)
-        else:
-            if not input_value and action.required:
-                raise ValueError('Option %s is required when in no-input mode' % action.dest)
-            new_val = input_value
-            if action.dest == 'db':
-                action(parser, args, new_val, action.option_strings)
-                new_val = getattr(args, action.dest)
-        if action.dest == 'templates' and (new_val == 'no' or not os.path.isdir(new_val)):
-            new_val = False
-        if action.dest in ('bootstrap', 'starting_page'):
-            new_val = (new_val == 'yes')
-        setattr(args, action.dest, new_val)
+    args = _manage_args(parser,  args)
 
     # what do we want here?!
     # * if languages are given as multiple arguments, let's use it as is
@@ -334,3 +292,53 @@ def show_requirements(args):
     Prints the list of requirements according to the arguments provided
     """
     sys.stdout.write(compat.unicode(args.requirements))
+
+
+def _manage_args(parser,  args):
+    """
+    Checks and validate provided input
+    """
+    for item in data.CONFIGURABLE_OPTIONS:
+        action = parser._option_string_actions[item]
+        choices = default = ''
+        input_value = getattr(args, action.dest)
+        new_val = None
+        # cannot count this until we find a way to test input
+        if not args.noinput:  # pragma: no cover
+            if action.choices:
+                choices = ' (choices: %s)' % ', '.join(action.choices)
+            if input_value:
+                if type(input_value) == list:
+                    default = ' [default %s]' % ', '.join(input_value)
+                else:
+                    default = ' [default %s]' % input_value
+
+            while not new_val:
+                prompt = '%s%s%s: ' % (action.help, choices, default)
+                if action.choices in ('yes', 'no'):
+                    new_val = utils.query_yes_no(prompt)
+                else:
+                    new_val = compat.input(prompt)
+                new_val = compat.clean(new_val)
+                if not new_val and input_value:
+                    new_val = input_value
+                if new_val and action.dest == 'templates':
+                    if new_val != 'no' and not os.path.isdir(new_val):
+                        sys.stdout.write('Given directory does not exists, retry\n')
+                        new_val = False
+                if new_val and action.dest == 'db':
+                    action(parser, args, new_val, action.option_strings)
+                    new_val = getattr(args, action.dest)
+        else:
+            if not input_value and action.required:
+                raise ValueError('Option %s is required when in no-input mode' % action.dest)
+            new_val = input_value
+            if action.dest == 'db':
+                action(parser, args, new_val, action.option_strings)
+                new_val = getattr(args, action.dest)
+        if action.dest == 'templates' and (new_val == 'no' or not os.path.isdir(new_val)):
+            new_val = False
+        if action.dest in ('bootstrap', 'starting_page'):
+            new_val = (new_val == 'yes')
+        setattr(args, action.dest, new_val)
+    return args
