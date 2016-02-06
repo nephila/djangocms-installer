6d731ee992b9a3d6d56f5513ac8670122e39c4f8
Iacopo Spalletti <i.spalletti@nephila.it>
2016-02-06 13:11:49 +0100

    Use 'modern' urlpatterns

--- djangocms_installer/config/urls.py
+++ djangocms_installer/config/urls.py
@@ -3,14 +3,14 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from cms.sitemaps import CMSSitemap
 from django.conf import settings
-from django.conf.urls import *  # NOQA
+from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
-urlpatterns = i18n_patterns('',
+urlpatterns = i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),  # NOQA
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
         {'sitemaps': {'cmspages': CMSSitemap}}),
@@ -20,7 +20,7 @@ urlpatterns = i18n_patterns('',
 
 # This is only needed when using runserver.
 if settings.DEBUG:
-    urlpatterns = patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',  # NOQA
+    urlpatterns = [
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-        ) + staticfiles_urlpatterns() + urlpatterns  # NOQA
+        ] + staticfiles_urlpatterns() + urlpatterns
