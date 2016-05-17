e37b0ee4205b1e82cb6f12f89f47cf8834939388
Vadim Sikora <vadim.sikora@divio.ch>
2016-05-17 13:57:13 +0200

    fix condition

--- djangocms_installer/config/__init__.py
+++ djangocms_installer/config/__init__.py
@@ -285,7 +285,7 @@ def parse(args):
                     requirements.extend(data.REQUIREMENTS['plugins-basic-master'])
             if cms_version == 3:
                 requirements.extend(data.REQUIREMENTS['ckeditor-3.0'])
-            if cms_version >= 3.3 or cms_version == 'rc':
+            elif cms_version >= 3.3 or cms_version == 'rc':
                 requirements.extend(data.REQUIREMENTS['ckeditor-3.3'])
             elif cms_version >= 3.2:
                 requirements.extend(data.REQUIREMENTS['ckeditor-3.2'])
